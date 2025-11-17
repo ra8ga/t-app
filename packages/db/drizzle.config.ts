90da38b9fea2c2ffc28f910fbdc7c38238440cf4
Rafal EF <you@example.com>
2025-11-17 14:07:01 +0100

    feat(db): uruchamianie Drizzle Studio lokalnie i zdalnie + aliasy portowe; konfiguracja LOCAL_DB_PATH i d1-http

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -5,15 +5,28 @@ dotenv.config({
 	path: "../../apps/server/.env",
 });
 
-export default defineConfig({
-	schema: "./src/schema",
-	out: "./src/migrations",
-	// DOCS: https://orm.drizzle.team/docs/guides/d1-http-with-drizzle-kit
-	dialect: "sqlite",
-	driver: "d1-http",
-	dbCredentials: {
-		accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
-		databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
-		token: process.env.CLOUDFLARE_D1_TOKEN!,
-	},
-});
+const LOCAL_DB_PATH = String(process.env.LOCAL_DB_PATH || "");
+
+export default defineConfig(
+	LOCAL_DB_PATH
+		? {
+			schema: "./src/schema",
+			out: "./src/migrations",
+			dialect: "sqlite",
+			dbCredentials: {
+				url: LOCAL_DB_PATH,
+			},
+		}
+		: {
+			schema: "./src/schema",
+			out: "./src/migrations",
+			// DOCS: https://orm.drizzle.team/docs/guides/d1-http-with-drizzle-kit
+			dialect: "sqlite",
+			driver: "d1-http",
+			dbCredentials: {
+				accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
+				databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
+				token: process.env.CLOUDFLARE_D1_TOKEN!,
+			},
+		}
+);
