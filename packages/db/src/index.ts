225e1e87c771be586cdccd204cb9b2bf0653e0a4
Rafal EF <you@example.com>
2025-11-24 15:42:45 +0100

    Pass schema to drizzle instance in createDb

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,6 +1,9 @@
 import { drizzle } from 'drizzle-orm/d1';
 import type { D1Database } from '@cloudflare/workers-types';
+import * as authSchema from './schema/auth';
+import * as adopsiakSchema from './schema/adopsiak';
 
-export const createDb = (d1: D1Database) => drizzle(d1);
+const schema = { ...authSchema, ...adopsiakSchema };
+
+export const createDb = (d1: D1Database) => drizzle(d1, { schema });
 export type DB = ReturnType<typeof createDb>;
-
